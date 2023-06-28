3f49b5b738b08ea88233e8ec0cf87541c1821f2a
Hassan <hassansmallah@gmail.com>
2023-06-28 14:49:43 +0200

    create post and get posts

--- routers/blog_post_router.py
+++ routers/blog_post_router.py
@@ -2,16 +2,32 @@
 
 from fastapi import APIRouter, Depends, HTTPException
 from motor.motor_asyncio import AsyncIOMotorDatabase
+
 from app.database import get_database
+from models.blog_post_model import BlogPost
 
 router = APIRouter()
 
 
 @router.get('/posts')
 async def get_posts(db: AsyncIOMotorDatabase = Depends(get_database)):
-    """ use get_database to connect to monogo"""
+    """ use get_database to connect to mongo """
 
-    cursor = db.posts.find()
-    posts = await cursor.to_list(length=None)  # Convert cursor to a list
+    posts = []
+    async for post in db.posts.find():
+        # Convert ObjectId to string
+        post["_id"] = str(post["_id"])
 
-    return {"posts": posts}
+        posts.append(post)
+
+    return posts
+
+
+@router.post('/create_post')
+async def create_post(post: BlogPost, db: AsyncIOMotorDatabase = Depends(get_database)):
+    """ get data and save it to mongo """"
+
+    data = post.dict()
+
+    result = await db.posts.insert_one(data)
+    return {"message": "Post created", "post_id": str(result.inserted_id)}
