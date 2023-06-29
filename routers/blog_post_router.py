cf647d9e25f0a0e80aaf6ca657b0eb21dea2e527
Hassan <hassansmallah@gmail.com>
2023-06-29 22:36:42 +0200

    get one post

--- routers/blog_post_router.py
+++ routers/blog_post_router.py
@@ -1,5 +1,7 @@
 # endpoints for blog post are written here
 
+from bson import ObjectId
+
 from fastapi import APIRouter, Depends, HTTPException
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
@@ -9,6 +11,23 @@ from models.blog_post_model import BlogPost
 router = APIRouter()
 
 
+@router.get('/posts/{post_id}')
+async def get_post(post_id: str, db: AsyncIOMotorDatabase = Depends(get_database)):
+    """ using post_id get a post """
+
+    # try to create ObjectId and get post
+    try:
+        post = await db.posts.find_one({'_id': ObjectId(post_id)})
+    except:
+        return HTTPException(status_code=400, detail='Incorrect post_id')
+
+    if not post:
+        return HTTPException(status_code=400, detail='Post not found')
+
+    post['_id'] = str(post['_id'])
+    return post
+
+
 @router.get('/posts')
 async def get_posts(db: AsyncIOMotorDatabase = Depends(get_database)):
     """ use get_database to connect to mongo """
@@ -16,7 +35,7 @@ async def get_posts(db: AsyncIOMotorDatabase = Depends(get_database)):
     posts = []
     async for post in db.posts.find():
         # Convert ObjectId to string
-        post["_id"] = str(post["_id"])
+        post['_id'] = str(post['_id'])
 
         posts.append(post)
 
@@ -25,9 +44,9 @@ async def get_posts(db: AsyncIOMotorDatabase = Depends(get_database)):
 
 @router.post('/create_post')
 async def create_post(post: BlogPost, db: AsyncIOMotorDatabase = Depends(get_database)):
-    """ get data and save it to mongo """"
+    """ get data and save it to mongo """
 
     data = post.dict()
 
     result = await db.posts.insert_one(data)
-    return {"message": "Post created", "post_id": str(result.inserted_id)}
+    return {'message': 'Post created', 'post_id': str(result.inserted_id)}
